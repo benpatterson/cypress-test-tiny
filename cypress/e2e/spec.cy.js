8836ac5832c57418b08b1e0057ceee073e7f0c13
Ben Patterson <2004678+benpatterson@users.noreply.github.com>
2026-04-10 17:00:40 -0400

    cy prompt example.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,4 +3,12 @@ describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('does cyprompt', () => {
+    cy.prompt([
+      'visit example.cypress.io',
+      'click on filter',
+      'you should see ".eq()"'
+    ])
+  })
 })
